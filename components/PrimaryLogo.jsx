6767db98b73a9fec28c1826d955d48bd05d5f133
Divyank Shah <divyank.shah.2016@gmail.com>
2023-01-15 05:04:42 -0800

    setup

---
+++ components/PrimaryLogo.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const PrimaryLogo = () => {
+  return <div>PrimaryLogo</div>;
+};
+
+export default PrimaryLogo;
---
+++ pages/dashboard.js
@@ -0,0 +1,25 @@
+import Filters from "@/components/Filters";
+import Problems from "@/components/Problems";
+import Progress from "@/components/Progress";
+import Search from "@/components/Search";
+import React from "react";
+import { Row, Col } from "react-bootstrap";
+
+const dashboard = () => {
+  return (
+    <div>
+      <Row>
+        <Col xl={4}>
+          <Filters />
+          <Progress />
+        </Col>
+        <Col xl={8}>
+          <Search />
+          <Problems />
+        </Col>
+      </Row>
+    </div>
+  );
+};
+
+export default dashboard;
