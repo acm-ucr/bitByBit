28699ca81510cc840ccd1007b3e1a8064bcacc15
Cindy <cho102@ucr.edu>
2023-01-18 12:54:09 -0800

    created basic structure

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -1,7 +1,14 @@
 import React from "react";
 
 const Filters = () => {
-  return <div>Filters</div>;
+  return <div>
+    <Container className="bg-black text-white rounded-[57px]">
+      <h1>Filter</h1>
+      <h3>clear all</h3>
+      <h2>Tags</h2>
+      <h2>Difficulty</h2>
+    </Container>
+  </div>;
 };
 
 export default Filters;
