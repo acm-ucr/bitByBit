40a22e9be774e975083eb5350b902e1334674534
Jasmita Yechuri <jasmita@Jasmitas-MacBook-Pro.local>
2023-01-22 14:09:36 -0800

    welcome components

--- components/Welcome.jsx
+++ components/Welcome.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const Welcome = () => {
-  return <div>Welcome</div>;
+  return (
+    <div className="bg-black w-full">
+      <div className="flex flex-col items-center pt-32">
+        <div className="text-white text-5xl font-bold mb-5">Welcome!</div>
+      </div>
+      <div className="flex flex-col items-center pb-32">
+        <button className="text-black text-2xl mb-4 rounded-full w-96 h-20 ring-4 ring-white font-bold bg-blue-300 ">
+          Get Started
+        </button>
+        <button className="text-white text-2xl rounded-full w-96 h-20 ring-4 ring-white font-bold bg-black-500 ">
+          I already have an account
+        </button>
+      </div>
+    </div>
+  );
 };
 
 export default Welcome;
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,8 @@
-import PrimaryLogo from "@/components/PrimaryLogo";
 import Welcome from "@/components/Welcome";
 
 const Index = () => {
   return (
     <div className="flex justify-center items-center">
-      <PrimaryLogo />
       <Welcome />
     </div>
   );
