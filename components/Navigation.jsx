f8251b5cd78f430892da208154d553864a61be3a
Sreeja Pagireddy <spagi@ucr.edu>
2023-01-23 17:35:17 -0800

    made a change

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -5,7 +5,7 @@ const Navigation = () => {
     <div className="w-full h-16 flex justify-center items-center bg-red-500">
       <div className="w-11/12 flex">
         <p className="m-0 p-0">bitByBIT</p>
-        <div className="">
+        <div>
           <p className="m-0 p-0">Problems</p>
         </div>
       </div>
