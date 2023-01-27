01e2f1b0bb2938c2bf925b0e13486b7f78db1679
Sreeja Pagireddy <spagi@ucr.edu>
2023-01-26 23:33:50 -0800

    part

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -2,11 +2,11 @@ import React from "react";
 
 const Navigation = () => {
   return (
-    <div className="w-full h-16 flex justify-center items-center bg-red-500">
+    <div className="w-full h-16 flex justify-left items-center bg-black text-white">
       <div className="w-11/12 flex">
-        <p className="m-0 p-0">bitByBIT</p>
+        <p className="m-0 p-0 text-3xl px-3 ">bitByBIT</p>
         <div>
-          <p className="m-0 p-0">Problems</p>
+          <p className="m-0 p-1 text-2xl px-3">Problems</p>
         </div>
       </div>
     </div>
