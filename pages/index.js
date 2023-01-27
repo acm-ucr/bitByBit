2d45e3fd3399ae1484eb9350ce9f932b6847bb18
Divyank Shah <divyank.shah.2016@gmail.com>
2023-01-27 14:34:44 -0800

    formatting

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,5 @@
 const Index = () => {
-  return (
-    <div className="flex justify-center items-center">
-    </div>
-  );
+  return <div className="flex justify-center items-center"></div>;
 };
 
 export default Index;
