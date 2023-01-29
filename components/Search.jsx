ded71cf5ae063bd6fd67cc6abcdc195cec4896b5
Rahul Kulkarni <rahulinsd@gmail.com>
2023-01-28 16:14:54 -0800

    styling

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,7 +1,56 @@
-import React from "react";
+import React, {useState} from "react";
 
 const Search = () => {
-  return <div>Search</div>;
+
+  const [searchInput, setSearchInput] = useState("");
+
+  const handleChange = (e) => {
+    e.preventDefault();
+    setSearchInput(e.target.value);
+  };
+
+  return (
+    <div id = "searchQuestions">
+      <input
+        type="search"
+        placeholder="search questions"
+        onChange={handleChange}
+        value={searchInput} 
+        // <style>
+        //   {
+        //     .searchBar
+        //     {
+        //       back
+        //     }
+        //   }
+        // </style>
+        
+        
+        // style = {border = 60}
+
+        // style = {border:5}
+        
+        // style={{
+        //   .oval {
+
+        //   }
+        // }}
+
+        // <style type="text/css">
+        //   .oval {
+        //     border-radius: 50%
+
+        //   }
+        // </style>
+        />
+        <button style = {{color: "red", background: "a84909"}}>
+          
+          random
+        </button>
+      
+    </div>
+  );
 };
 
 export default Search;
+
