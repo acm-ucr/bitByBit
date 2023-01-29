49c3be343f9b8602d547a199886ea45409ba527c
Divyank Shah <divyank.shah.2016@gmail.com>
2023-01-28 20:55:19 -0800

    add layout

---
+++ components/Layout.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Navigation from "./Navigation";
+
+const Layout = ({ children }) => {
+  return (
+    <>
+      <Navigation />
+      {children}
+    </>
+  );
+};
+
+export default Layout;
--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,22 @@
-/* eslint-disable new-cap */
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 // eslint-disable-next-line camelcase
 import { Readex_Pro } from "@next/font/google";
+import Layout from "@/components/Layout";
 
+// eslint-disable-next-line new-cap
 const readex = Readex_Pro({
   subsets: ["latin"],
   variable: "--font-readex",
-  weight: "500",
+  weight: ["200", "300", "400", "500", "600", "700"],
 });
 
 export default function App({ Component, pageProps }) {
   return (
     <main className={`${readex.variable}`}>
-      <Component {...pageProps} />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
     </main>
   );
 }
