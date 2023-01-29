6011826a448974dc34fa124bd7f7acd12a397ae2
Divyank Shah <divyank.shah.2016@gmail.com>
2023-01-28 18:45:34 -0800

    setup structure

--- components/Search.jsx
+++ components/Search.jsx
@@ -1,59 +1,21 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
+import { FaSearch } from "react-icons/fa";
 
 const Search = () => {
-
   const [searchInput, setSearchInput] = useState("");
 
-  const handleChange = (e) => {
-    e.preventDefault();
-    setSearchInput(e.target.value);
-  };
-
   return (
-    <div id = "searchQuestions">
+    <div className="flex justify-between items-center bg-red-500">
       <input
+        className="text-black"
         type="search"
         placeholder="search questions"
-        onChange={handleChange}
-        value={searchInput} 
-        
-
-        style = {{color: "red"}}
-        // <style>
-        //   {
-        //     .searchBar
-        //     {
-        //       back
-        //     }
-        //   }
-        // </style>
-        
-        
-        // style = {border = 60}
-
-        // style = {border:5}
-        
-        // style={{
-        //   .oval {
-
-        //   }
-        // }}
-
-        // <style type="text/css">
-        //   .oval {
-        //     border-radius: 50%
-
-        //   }
-        // </style>
-        />
-        <button style = {{}}>
-          
-          random
-        </button>
-      
+        onChange={(e) => setSearchInput(e.target.value)}
+        value={searchInput}
+      />
+      <FaSearch className="text-3xl text-black hover:!text-blue-500 cursor-pointer" />
     </div>
   );
 };
 
 export default Search;
-
