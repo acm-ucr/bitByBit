30c3221c47e29dd784ac2f5f49bebddea26ea849
Divyank Shah <divyank.shah.2016@gmail.com>
2023-01-30 15:44:36 -0800

    finalize search

--- components/Search.jsx
+++ components/Search.jsx
@@ -5,15 +5,17 @@ const Search = () => {
   const [searchInput, setSearchInput] = useState("");
 
   return (
-    <div className="flex justify-between items-center bg-red-500">
-      <input
-        className="text-black"
-        type="search"
-        placeholder="search questions"
-        onChange={(e) => setSearchInput(e.target.value)}
-        value={searchInput}
-      />
-      <FaSearch className="text-3xl text-black hover:!text-blue-500 cursor-pointer" />
+    <div className="flex justify-between items-center">
+      <div className="bg-white flex border-black border-2 rounded-full p-2">
+        <FaSearch className="text-3xl text-black hover:!text-blue-500 cursor-pointer mx-2" />
+        <input
+          className="text-black outline-none"
+          type="search"
+          placeholder="search questions"
+          onChange={(e) => setSearchInput(e.target.value)}
+          value={searchInput}
+        />
+      </div>
     </div>
   );
 };
