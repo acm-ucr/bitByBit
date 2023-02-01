2ad2fbe047bfdc1057f3c01ecfce5eeb6c61545b
mika shanela <mikashanela@Mikas-MacBook-Pro.local>
2023-02-01 00:20:32 -0800

    added problem solved component #39

---
+++ components/ProblemsCategory.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const ProblemCategory = ({ difficulty, solved, total, color }) => {
+  return (
+    <div>
+      <div className="flex flex-wrap self-center justify-between text-2xl py-2">
+        <div className="font-medium">{difficulty}</div>
+        <div className="flex items-center rounded-xl font-medium">
+          <div className="font-normal bg-black rounded-l-lg text-white text-2xl px-2">
+            {solved}
+          </div>
+          <div className={`font-normal ${color} rounded-r-lg text-2xl px-2`}>
+            {total}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default ProblemCategory;
--- components/ProblemsSolved.jsx
+++ components/ProblemsSolved.jsx
@@ -1,7 +1,44 @@
 import React from "react";
+import ProblemsCategory from "@/components/ProblemsCategory";
 
 const ProblemsSolved = () => {
-  return <div>ProblemsSolved</div>;
+  return (
+    <div className="bg-code-gray rounded-3xl w-6/12 p-6 m-6 font-medium items-center place-content-center">
+      <p className="text-3xl mb-4">Problems Solved</p>
+      <div className="flex justify-center">
+        <div className="bg-code-gray w-40 h-40 rounded-full flex flex-column justify-center items-center border-black mb-6 border-8 border-r-code-green">
+          <p className="font-semibold text-4xl mb-0">15</p>
+          <p className="font-normal text-3xl mt-0">solved</p>
+        </div>
+      </div>
+      <div>
+        {
+          <ProblemsCategory
+            difficulty="Easy"
+            solved={8}
+            total={20}
+            color="bg-code-lime"
+          />
+        }
+        {
+          <ProblemsCategory
+            difficulty="Medium"
+            solved={4}
+            total={15}
+            color="bg-code-yellow"
+          />
+        }
+        {
+          <ProblemsCategory
+            difficulty="Hard"
+            solved={3}
+            total={15}
+            color="bg-code-pink"
+          />
+        }
+      </div>
+    </div>
+  );
 };
 
 export default ProblemsSolved;
