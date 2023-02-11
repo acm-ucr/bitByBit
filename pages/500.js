3d0f9607fbddeea90f880ea7a62a3e4e4b9cd00d
Sreeja Pagireddy <spagi@ucr.edu>
2023-02-11 14:21:15 -0800

    500 page

---
+++ pages/500.js
@@ -0,0 +1,13 @@
+export default function Custom404() {
+  return (
+    <div class="flex h-screen">
+      <div class="m-auto">
+        <h1 class="text-9xl font-readex text-code-black">500</h1>
+
+        <p class="text-2xl font-readex indent-8 text-code-black">
+          Page Not Found
+        </p>
+      </div>
+    </div>
+  );
+}
