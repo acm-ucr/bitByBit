154f21bd29a82cd4ff1083d0088b48af4a46c3ea
Justin Yee <justiny@solidprofessor.com>
2023-02-13 15:22:46 -0800

    linting stuff

--- components/ProblemRecord.jsx
+++ components/ProblemRecord.jsx
@@ -1,7 +1,9 @@
 import React from "react";
 
 const ProblemRecord = () => {
-  return <div className="bg-code-black text-code-white h-screen">ProblemRecord</div>;
+  return (
+    <div className="bg-code-black text-code-white h-screen">ProblemRecord</div>
+  );
 };
 
 export default ProblemRecord;
