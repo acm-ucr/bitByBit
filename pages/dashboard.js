f3500a4e315175f7f8504783c5122d8c9aee5cb6
theo <theobfernandez@gmail.com>
2023-02-13 20:54:41 -0800

    Run prettier

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -9,10 +9,7 @@ const dashboard = () => {
   return (
     <div className="bg-code-black h-screen">
       <Row className="bg-code-black">
-        <Col
-          xl={4}
-          className="flex gap-4 justify-center items-center flex-col"
-        >
+        <Col xl={4} className="flex gap-4 justify-center items-center flex-col">
           <Filters />
           <Progress />
         </Col>
