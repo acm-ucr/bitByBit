405e856dc0ee09c8d1ef559d9354703c71c012bb
theo <theobfernandez@gmail.com>
2023-02-13 20:24:34 -0800

    Fix dashboard alignment

--- pages/dashboard.js
+++ pages/dashboard.js
@@ -9,11 +9,14 @@ const dashboard = () => {
   return (
     <div className="bg-code-black h-screen">
       <Row className="bg-code-black">
-        <Col xl={4} className="flex justify-center items-center flex-col">
+        <Col
+          xl={4}
+          className="flex ml-4 gap-4 justify-center items-center flex-col"
+        >
           <Filters />
           <Progress />
         </Col>
-        <Col xl={8} className="flex justify-center items-center flex-col">
+        <Col xl={7} className="flex justify-center items-left flex-col">
           <Search />
           <Problems />
         </Col>
