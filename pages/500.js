aad1b50283a8a3c17d48d6d76e3fd787a00cb780
khandrew1 <54488379+khandrew1@users.noreply.github.com>
2023-03-05 00:31:52 -0800

    Fixed Warnings

--- pages/500.js
+++ pages/500.js
@@ -1,10 +1,10 @@
 export default function Custom404() {
   return (
-    <div class="flex h-screen">
-      <div class="m-auto">
-        <h1 class="text-9xl font-readex text-code-black">500</h1>
+    <div className="flex h-screen">
+      <div className="m-auto">
+        <h1 className="text-9xl font-readex text-code-black">500</h1>
 
-        <p class="text-2xl font-readex indent-8 text-code-black">
+        <p className="text-2xl font-readex indent-8 text-code-black">
           Server Error
         </p>
       </div>
