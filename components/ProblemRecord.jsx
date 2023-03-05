a5357ccc010e780589e8c2fe5e525c19c4d75b56
theo <theobfernandez@gmail.com>
2023-03-04 19:22:53 -0800

    Implement Progress Record component

--- components/ProblemRecord.jsx
+++ components/ProblemRecord.jsx
@@ -1,4 +1,5 @@
 import React from "react";
+import ProgressCircle from "./ProgressCircle";
 
 const dummyProgrammingProblems = [
   {
@@ -38,36 +39,6 @@ const dummyProgrammingProblems = [
     status: "in-progress",
   },
 ];
-const ProgressCircle = () => {
-  const radius = 7;
-  const circleWidth = 40;
-  const dashArray = 2 * Math.PI * radius;
-  const dashOffset = dashArray - dashArray* 62/100;
-  return (
-    <svg
-      width={circleWidth}
-      height={circleWidth}
-      viewBox={`0 0 ${circleWidth} ${circleWidth}`}
-    >
-      <circle
-        cx={circleWidth / 2}
-        cy={circleWidth / 2}
-        r={radius}
-        strokeWidth="19px"
-        className="fill-none stroke-code-white"
-      />
-      <circle
-        cx={circleWidth / 2}
-        cy={circleWidth / 2}
-        r={radius}
-        strokeWidth="14px"
-        style={{ strokeDasharray: dashArray, strokeDashoffset: dashOffset}}
-        className="fill-none stroke-code-darkerpurple"
-        transform={`rotate(45 ${circleWidth / 2} ${circleWidth / 2})`}
-      />
-    </svg>
-  );
-};
 
 const handleProgrammingProblems = (problem, index) => {
   const difficultyColors = {
@@ -85,7 +56,7 @@ const handleProgrammingProblems = (problem, index) => {
     >
       <div className="flex flex-col">
         <div className="text-2xl mb-2">{problem.title}</div>
-        <div className="flex justify-between">
+        <div className="flex">
           {problem.tags.map((element, tagsIndex) => (
             <div
               className={`mr-1 px-1 ${
@@ -98,18 +69,22 @@ const handleProgrammingProblems = (problem, index) => {
           ))}
         </div>
       </div>
-      <div
-        className={`${
-          difficultyColors[problem.difficulty]
-        } text-2xl text-center`}
-      >
-        {problem.difficulty}
+      <div className="w-1/2 justify-between flex ">
+        <div
+          className={`${
+            difficultyColors[problem.difficulty]
+          } text-2xl text-center`}
+        >
+          {problem.difficulty}
+        </div>
+        <div>
+        {problem.status === "completed" ? (
+          <ProgressCircle completed={true} index={index} />
+        ) : (
+          <ProgressCircle completed={false} index={index} />
+        )}
+        </div>
       </div>
-      {problem.status === "completed" ? (
-        <ProgressCircle />
-      ) : (
-        <div className="bg-code-white rounded-full flex flex-column justify-center items-center mb-6 border-8"></div>
-      )}
     </div>
   );
 };
@@ -117,25 +92,25 @@ const handleProgrammingProblems = (problem, index) => {
 const ProblemRecord = () => {
   return (
     <div className="py-3 pr-4 font-readex w-full h-screen text-code-white">
-      <div className="flex gap-3">
+      <div className="flex gap-3 mb-3">
         <div className="font-bold text-2xl">Problem Record</div>
         <div className="flex gap-1">
-          <div className="hover:cursor-pointer px-3 self-center border-code-darkpurple border-2 rounded-full">
+          <div className="hover:cursor-pointer px-3 self-center bg-code-darkpurple border-code-darkpurple border-2 rounded-full">
             all
           </div>
-          <div className="px-3 self-center border-code-darkpurple border-2 rounded-full">
+          <div className="hover:cursor-pointer px-3 self-center border-code-darkpurple border-2 rounded-full">
             completed
           </div>
-          <div className="px-3 self-center border-code-darkpurple border-2 rounded-full">
+          <div className="hover:cursor-pointer px-3 self-center  border-code-darkpurple border-2 rounded-full">
             in progress
           </div>
         </div>
       </div>
       <div className="flex justify-between">
         <div className="text-xl">Title</div>
-        <div className="flex">
+        <div className="flex gap-10 w-1/2 justify-between">
           <div className="text-xl">Difficulty</div>
-          <div className="text-xl">Status</div>
+          <div className="text-xl mr-4">Status</div>
         </div>
       </div>
       <hr className="border-2 opacity-100 border-white" />
