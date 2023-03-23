815ba16120d25fac9a9627ce46a4b5414e5a41ad
khandrew1 <54488379+khandrew1@users.noreply.github.com>
2023-03-23 00:30:29 -0700

    implemented clear all functionallity into FIlters component

--- components/Filter.jsx
+++ components/Filter.jsx
@@ -1,19 +1,21 @@
-import React, { useState } from "react";
+import React, { useState, useMemo } from "react";
 
 const Filter = ({ color, name, setFiltered, filtered }) => {
-  const [active, setActive] = useState(0);
+  const isActive = useMemo(() => {
+    return filtered.includes(name);
+  }, [filtered, name]);
+
   const handleClick = () => {
-    if (!active) {
-      setActive(1);
+    if (!isActive) {
       setFiltered([...filtered, name]);
     } else {
-      setActive(0);
       setFiltered(filtered.filter((item) => item !== name));
     }
   };
+
   return (
     <div className="hover:cursor-pointer select-none" onClick={handleClick}>
-      {active ? (
+      {isActive ? (
         <div
           className={` flex border-code-white text-2xl font-extralight border-2 px-2 py-1 ${color} rounded-full text-code-black`}
         >
