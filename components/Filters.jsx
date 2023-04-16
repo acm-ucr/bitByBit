a3a009966272250f579c2ab1999578ada0c09834
Jonathan Le <jonathanle062303@gmail.com>
2023-04-15 23:28:16 -0700

    Used prettier

--- components/Filters.jsx
+++ components/Filters.jsx
@@ -4,39 +4,39 @@ import Filter from "./Filter";
 const tags = [
   {
     name: "variables",
-    color: "bg-code-yellow"
+    color: "bg-code-yellow",
   },
   {
     name: "arrays",
-    color: "bg-code-lime"
+    color: "bg-code-lime",
   },
   {
     name: "conditionals",
-    color: "bg-code-lightpink"
+    color: "bg-code-lightpink",
   },
   {
     name: "loops",
-    color: "bg-code-yellow"
+    color: "bg-code-yellow",
   },
   {
     name: "functions",
-    color: "bg-code-blue"
-  }
+    color: "bg-code-blue",
+  },
 ];
 
 const difficulties = [
   {
     name: "easy",
-    color: "bg-code-lightgreen"
+    color: "bg-code-lightgreen",
   },
   {
     name: "medium",
-    color: "bg-code-yellow"
+    color: "bg-code-yellow",
   },
   {
     name: "hard",
-    color: "bg-code-lightpink"
-  }
+    color: "bg-code-lightpink",
+  },
 ];
 
 const Filters = () => {
