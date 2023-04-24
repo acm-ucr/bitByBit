b9eec72a87870df12dede8cdca691e6c703550b2
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-04-24 01:46:14 -0700

    Implemented console component

--- components/Console.jsx
+++ components/Console.jsx
@@ -1,7 +1,29 @@
 import React from "react";
+import { RxCaretUp } from "react-icons/rx";
 
 const Console = () => {
-  return <div className="bg-red-500">Console</div>;
+  return (
+    <div className="p-2.5 text-code-white flex-col font-readex">
+      <div className="flex flex-row gap-4 font-normal">
+        <button className="underline underline-offset-4">Testcase</button>
+        <button className="">Result</button>
+      </div>
+      <div className="h-40"></div>
+      <div className="flex flex-row gap-4 justify-between">
+        <button className="flex rounded-lg bg-code-darkpurple px-3 py-1">
+          Console <RxCaretUp className="text-2xl" />
+        </button>
+        <div className="flex text-code-black gap-2">
+          <button className="rounded-full bg-code-white font-bold px-3">
+            Run
+          </button>
+          <button className="rounded-full bg-code-lime font-bold px-3">
+            Submit
+          </button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Console;
