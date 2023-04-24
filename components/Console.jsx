a6deabc9760531a3ebb395c8e8311bca6383b1cb
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-04-24 02:12:37 -0700

    Added a state toggle to console

--- components/Console.jsx
+++ components/Console.jsx
@@ -1,14 +1,43 @@
 import React from "react";
 import { RxCaretUp } from "react-icons/rx";
+import { useState } from "react";
+
+const ConsoleStateToggle = ({ state, onUpdateState }) => {
+  return (
+    <>
+      <div className="flex flex-row gap-4 font-normal">
+        <div
+          className={`${
+            state === 0
+              ? "underline underline-offset-4 cursor-pointer"
+              : "cursor-pointer"
+          }`}
+          onClick={() => onUpdateState(0)}
+        >
+          Testcase
+        </div>
+        <div
+          className={`${
+            state === 1
+              ? "underline underline-offset-4 cursor-pointer"
+              : "cursor-pointer"
+          }`}
+          onClick={() => onUpdateState(1)}
+        >
+          Result
+        </div>
+      </div>
+    </>
+  );
+};
 
 const Console = () => {
+  const [state, setState] = useState(0);
   return (
     <div className="p-2.5 text-code-white flex-col font-readex">
-      <div className="flex flex-row gap-4 font-normal">
-        <button className="underline underline-offset-4">Testcase</button>
-        <button className="">Result</button>
-      </div>
-      <div className="h-40"></div>
+      <ConsoleStateToggle state={state} onUpdateState={setState} />
+      {state === 0 && <div className="h-40">.</div>}
+      {state === 1 && <div className="h-40">..</div>}
       <div className="flex flex-row gap-4 justify-between">
         <button className="flex rounded-lg bg-code-darkpurple px-3 py-1">
           Console <RxCaretUp className="text-2xl" />
