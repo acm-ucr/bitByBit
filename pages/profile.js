4b56110e64f24d168fe7ea7b1a69a0b43053c35c
khandrew1 <54488379+khandrew1@users.noreply.github.com>
2023-04-26 22:35:22 -0700

    added ordinal date suffix into joinDate prop

--- pages/profile.js
+++ pages/profile.js
@@ -6,9 +6,39 @@ import Col from "react-bootstrap/Col";
 import Profile from "@/components/Profile";
 import UserContext from "@/components/UserContext";
 import { useContext } from "react";
+import { Timestamp } from "firebase/firestore";
 
 const ProfilePage = () => {
   const { user } = useContext(UserContext);
+
+  const getDate = () => {
+    const timestamp = new Timestamp(
+      user.created.seconds,
+      user.created.nanoseconds
+    ).toDate();
+
+    const nthNumber = (number) => {
+      if (number > 3 && number < 21) return "th";
+      switch (number % 10) {
+        case 1:
+          return "st";
+        case 2:
+          return "nd";
+        case 3:
+          return "rd";
+        default:
+          return "th";
+      }
+    };
+
+    const day = timestamp.getDate();
+    const month = timestamp.toLocaleDateString("en-US", { month: "long" });
+    const year = timestamp.getFullYear();
+
+    const date = `${month} ${day}${nthNumber(day)}, ${year}`;
+
+    return date;
+  };
 
   return (
     <div className="bg-code-black">
@@ -19,10 +49,10 @@ const ProfilePage = () => {
               src={user.image}
               fullName={user.name}
               userName={user.username}
-              joinDate={user.created}
+              joinDate={getDate()}
             />
           ) : (
-            <Profile joinDate="April 1st 2023" />
+            <Profile />
           )}
           <ProblemsSolved />
         </Col>
