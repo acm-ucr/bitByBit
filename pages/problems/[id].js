e62565350444b063c3f0fff71e42aca65c79d805
khandrew1 <54488379+khandrew1@users.noreply.github.com>
2023-05-04 01:12:46 -0700

    Added first divider

--- pages/problems/[id].js
+++ pages/problems/[id].js
@@ -5,45 +5,134 @@ import ProblemSubmission from "@/components/ProblemSubmission";
 import CodeEditor from "@/components/CodeEditor";
 import Console from "@/components/Console";
 
-import { useState } from "react";
+import { useState, useRef } from "react";
+
+const useResize = (containerRef, panelRef, initialWidth, minWidth = 0) => {
+  const [panelWidth, setPanelWidth] = useState(initialWidth);
+
+  const onResizeStart = () => {
+    if (panelRef.current) {
+      panelRef.current.style.pointerEvents = "none";
+      panelRef.current.style.userSelect = "none";
+    }
+    if (containerRef.current) {
+      containerRef.current.classList.add("resizing");
+      containerRef.current.style.cursor = "ew-resize";
+    }
+    window.addEventListener("pointermove", onResize);
+    window.addEventListener("pointerup", onResizeEnd);
+  };
+
+  const onResize = (e) => {
+    if (containerRef.current) {
+      const bounds = containerRef.current.getBoundingClientRect();
+      const newWidth = e.clientX - bounds.left;
+      if (newWidth >= minWidth) {
+        setPanelWidth(newWidth);
+      } else {
+        setPanelWidth(minWidth);
+      }
+    }
+  };
+
+  const onResizeEnd = () => {
+    if (panelRef.current) {
+      panelRef.current.style.pointerEvents = "auto";
+      panelRef.current.style.userSelect = "auto";
+    }
+    if (containerRef.current) {
+      containerRef.current.classList.remove("resizing");
+      containerRef.current.style.cursor = "auto";
+    }
+    window.removeEventListener("pointermove", onResize);
+    window.removeEventListener("pointerup", onResizeEnd);
+  };
+
+  return { panelWidth, onResizeStart };
+};
 
 const Problems = () => {
   const [state, setState] = useState(0);
 
+  const containerRef = useRef();
+  const panelRef = useRef();
+  const handleWidth = 16;
+  const maxContainerWidth = window.innerWidth;
+
+  const { panelWidth, onResizeStart } = useResize(
+    containerRef,
+    panelRef,
+    maxContainerWidth / 2
+  );
+
   return (
-    <div className=" flex">
-      <div className="w-1/2">
-        <ProblemStateToggle state={state} onUpdateState={setState} />
-        {state === 0 && (
-          <ProblemDescription
-            name="TEST"
-            difficulty="HARD"
-            tags={["arrays", "hashmap"]}
-            description="TEST DESCRIPTION"
-            example="TEST EXAMPLE"
-            constraints={["constrain 1", "constrain 2"]}
-          />
-        )}
-        {state === 1 && (
-          <ProblemSolution
-            entries={[
-              {
-                methodName: "TEST METHOD",
-                description: "TEST DESCRIPTION",
-                implementation: "TEST IMPLEMENTATION",
-                timeComplexity: "TEST TIME COMPLEXITY",
-                spaceComplexity: "TEST SPACE COMPLEXITY",
-              },
-            ]}
-          />
-        )}
-        {state === 2 && <ProblemSubmission />}
+    <>
+      <div
+        ref={containerRef}
+        style={{
+          width: "100%",
+          maxWidth: `${maxContainerWidth}px`,
+          height: "400px",
+          display: "flex",
+        }}
+      >
+        <div
+          ref={panelRef}
+          style={{
+            position: "relative",
+            width: `${panelWidth}px`,
+            maxWidth: "100%",
+            height: "100%",
+            paddingRight: `${handleWidth}px`,
+            display: "flex",
+            "flex-direction": "column",
+          }}
+        >
+          <ProblemStateToggle state={state} onUpdateState={setState} />
+          {state === 0 && (
+            <ProblemDescription
+              name="TEST"
+              difficulty="HARD"
+              tags={["arrays", "hashmap"]}
+              description="TEST DESCRIPTION"
+              example="TEST EXAMPLE"
+              constraints={["constrain 1", "constrain 2"]}
+            />
+          )}
+          {state === 1 && (
+            <ProblemSolution
+              entries={[
+                {
+                  methodName: "TEST METHOD",
+                  description: "TEST DESCRIPTION",
+                  implementation: "TEST IMPLEMENTATION",
+                  timeComplexity: "TEST TIME COMPLEXITY",
+                  spaceComplexity: "TEST SPACE COMPLEXITY",
+                },
+              ]}
+            />
+          )}
+          {state === 2 && <ProblemSubmission />}
+
+          <div
+            style={{
+              position: "absolute",
+              top: "0",
+              right: "0",
+              height: "100%",
+              background: "white",
+              width: `${handleWidth}px`,
+              cursor: "ew-resize",
+            }}
+            onPointerDown={onResizeStart}
+          ></div>
+        </div>
+        <div className="flex flex-col flex-grow">
+          <CodeEditor />
+          <Console />
+        </div>
       </div>
-      <div className="w-1/2">
-        <CodeEditor />
-        <Console />
-      </div>
-    </div>
+    </>
   );
 };
 
