4c8eedfd393fd427ae86e06846188e9e03760ab1
daniel-u-c <74112469+daniel-u-c@users.noreply.github.com>
2023-05-08 01:14:51 -0700

    Formatting

--- components/CodeEditor.jsx
+++ components/CodeEditor.jsx
@@ -1,17 +1,17 @@
 import React from "react";
 import CodeContext from "./CodeContext";
 import { useContext } from "react";
-import CodeMirror from '@uiw/react-codemirror'
+import CodeMirror from "@uiw/react-codemirror";
 import { cpp } from "@codemirror/lang-cpp";
 import { javascript } from "@codemirror/lang-javascript";
 import { python } from "@codemirror/lang-python";
-import { aura } from "@uiw/codemirror-theme-aura"
+import { aura } from "@uiw/codemirror-theme-aura";
 
 const CodeEditor = () => {
   const { code, setCode } = useContext(CodeContext);
   console.log(code);
   const onChange = React.useCallback((value, viewUpdate) => {
-    console.log('value:', value);
+    console.log("value:", value);
     setCode(value);
   }, []);
   return (
