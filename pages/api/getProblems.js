f341b634628cbd7ef1527920fbc14ae97e5015e7
Divyank Shah <divyank.shah.2016@gmail.com>
2023-05-21 22:37:58 -0700

    add difficulty

--- pages/api/getProblems.js
+++ pages/api/getProblems.js
@@ -20,6 +20,7 @@ export default async function getProfileInfo(req, res) {
         { output: "[4, 5]", input: "[1, 2, 3, 4, 5]" },
         { input: "[6, 7, 8, 9, 0]", output: "[8, 9]" },
       ],
+      difficulty: "easy",
       description: "Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.  You may assume that each input would have exactly one solution, and you may not use the same element twice.  You can return the answer in any order.",
       solutions: [
         {
