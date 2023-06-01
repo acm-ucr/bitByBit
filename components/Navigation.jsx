8d5661caee2e7c37d419213b3a84f6dd1658665c
JasmitaY <jasmita99@gmail.com>
2023-06-01 13:03:43 -0700

    logo to dashboard when signed in

--- components/Navigation.jsx
+++ components/Navigation.jsx
@@ -17,7 +17,7 @@ const Navigation = () => {
       variant="light"
       className="bg-code-black font-semibold font-readex w-11/12 gap-x-5"
     >
-      <Navbar.Brand href="/" className="!text-code-white">
+      <Navbar.Brand href="/dashboard" className="!text-code-white">
         <div className="text-3xl">bitByBIT</div>
       </Navbar.Brand>
       {user && (
