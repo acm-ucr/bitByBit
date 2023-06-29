b72c7b26e610811bc48653ca347f6f18d9d15ae4
Val <vwong031@ucr.edu>
2023-06-28 23:21:56 -0700

    Change color of 'Accepted' and change font size of 'runtime'

--- components/Results.jsx
+++ components/Results.jsx
@@ -3,7 +3,10 @@ import React from "react";
 const Results = () => {
   return (
     <div>
-      <div>Accepted</div>
+      <div className="flex flex-row gap-4 font-normal">
+        <div className="text-lg text-code-green">Accepted</div>
+        <div className="text-sm">Runtime: 0ms</div>
+      </div>
       <div className="flex flex-row gap-4 font-normal">
         <button className="flex rounded-lg bg-code-darkpurple px-3 py-1">
           Case 1
