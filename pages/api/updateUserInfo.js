d3846167fe49f7159c2452b5ebd72cccd0df4a9c
Val <vwong031@ucr.edu>
2023-07-17 16:11:23 -0700

    ran prettier

--- pages/api/updateUserInfo.js
+++ pages/api/updateUserInfo.js
@@ -1,10 +1,10 @@
 import { db } from "../../firebase";
 import { doc, updateDoc } from "firebase/firestore";
-import { getServerSession } from "next-auth/react"
-import { authOptions } from "./auth/[...nextauth]"
+import { getServerSession } from "next-auth/react";
+import { authOptions } from "./auth/[...nextauth]";
 
 export default async function updateUserInfo(req, res) {
-  const session = await getServerSession(req, res, authOptions)
+  const session = await getServerSession(req, res, authOptions);
 
   if (session) {
     const userDocRef = doc(db, "users", req.body.uid);
@@ -18,9 +18,8 @@ export default async function updateUserInfo(req, res) {
         .status(500)
         .json({ error: "*Sorry, there was an error with your request" });
     }
+  } else {
+    res.status(401);
   }
-  else {
-    res.status(401)
-  }
-  res.end()
+  res.end();
 }
