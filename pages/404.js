1752accf4be6ae7f472742f4453e333cc5d7d448
jonathanle03 <jonathanle062303@gmail.com>
2023-07-22 16:45:01 -0700

    Fixed 404 and 500 rendering

--- pages/404.js
+++ pages/404.js
@@ -1,10 +1,10 @@
 export default function Custom404() {
   return (
-    <div className="flex h-screen">
+    <div className="flex h-full">
       <div className="m-auto">
-        <div className="text-9xl font-readex text-code-black">404</div>
+        <div className="text-9xl font-readex text-code-white">404</div>
 
-        <p className="text-2xl font-readex indent-8 text-code-black">
+        <p className="text-2xl font-readex indent-8 text-code-white">
           Page Not Found
         </p>
       </div>
