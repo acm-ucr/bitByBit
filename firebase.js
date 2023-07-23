b6fd456be4f603b1be0efec0f510b03a490b7900
jonathanle03 <jonathanle062303@gmail.com>
2023-07-22 17:06:27 -0700

    Removed firebase auth

--- firebase.js
+++ firebase.js
@@ -2,7 +2,7 @@
 import { initializeApp } from "firebase/app";
 // import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "firebase/firestore";
-import { getAuth } from "firebase/auth";
+// import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -23,5 +23,5 @@ const firebaseConfig = {
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-export const auth = getAuth(app);
+// export const auth = getAuth(app);
 // export const analytics = getAnalytics(app);
