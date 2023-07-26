cd8a5e6c197c14c2bdbb0da109453c23dc98a778
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2023-07-25 22:15:29 -0700

    Redirecting /user to dashboard

--- next.config.js
+++ next.config.js
@@ -1,6 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async redirects() {
+    return [
+      {
+        source: "/user",
+        destination: "/user/dashboard",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
