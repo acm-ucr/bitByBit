9aa5a88d85f17f30ae623ca5c314ae45a9118702
Jonathan Le <jonathanle062303@gmail.com>
2023-07-27 21:55:27 -0700

    Removed unused dependencies

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,9 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
 import CodeContext from "@/components/CodeContext";
 import "../styles/globals.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 import Layout from "@/components/Layout";
 import { SessionProvider } from "next-auth/react";
-import axios from "axios";
 
 // eslint-disable-next-line camelcase
 import { Readex_Pro } from "@next/font/google";
