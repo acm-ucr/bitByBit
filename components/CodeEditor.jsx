f940f3a7335ca52408c43abe863162537e84903b
Val <vwong031@ucr.edu>
2023-08-01 14:19:11 -0700

    boilerplate code added. function header should change with each problem, but the parameter does not change

--- components/CodeEditor.jsx
+++ components/CodeEditor.jsx
@@ -1,23 +1,37 @@
 import React from "react";
 import CodeContext from "./CodeContext";
-import { useContext, useCallback } from "react";
+import { useContext, useCallback, useEffect } from "react";
 import CodeMirror from "@uiw/react-codemirror";
 import { cpp } from "@codemirror/lang-cpp";
 // import { javascript } from "@codemirror/lang-javascript";
 // import { python } from "@codemirror/lang-python";
 import { aura } from "@uiw/codemirror-theme-aura";
 
-const CodeEditor = () => {
-  const { setCode } = useContext(CodeContext);
-  const onChange = useCallback((value) => {
-    setCode(value);
-  }, []);
+const CodeEditor = ({ problem }) => {
+  const { code, setCode } = useContext(CodeContext);
+
+  useEffect(() => {
+    if (problem) {
+      /* When a problem is selected, set the code editor content to the function header */
+      problem.functionHeader =
+        "def " + problem.title.toLowerCase().replaceAll(" ", "_") + "(nums): ";
+      setCode(problem.functionHeader);
+    }
+  }, [problem, setCode]);
+  const onChange = useCallback(
+    (value) => {
+      setCode(value);
+    },
+    [setCode]
+  );
+
   return (
     <div className="bg-code-darkerpurple h-full">
       <CodeMirror
         extensions={[cpp()]}
         // extensions={[python()]}
         // extensions={[javascript({ jsx: true })]}
+        value={code}
         onChange={onChange}
         theme={aura}
       />
