56c089d0a4ac657de6da39fdee57fa8685d1cd03
Sean Quiambao <91030482+toastmeal@users.noreply.github.com>
2023-08-21 15:12:14 -0700

    Change codemirror language to python

--- components/CodeEditor.jsx
+++ components/CodeEditor.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import CodeContext from "./CodeContext";
 import { useContext, useCallback } from "react";
 import CodeMirror from "@uiw/react-codemirror";
-import { cpp } from "@codemirror/lang-cpp";
+// import { cpp } from "@codemirror/lang-cpp";
 // import { javascript } from "@codemirror/lang-javascript";
-// import { python } from "@codemirror/lang-python";
+import { python } from "@codemirror/lang-python";
 import { aura } from "@uiw/codemirror-theme-aura";
 
 const CodeEditor = ({ problem }) => {
@@ -17,8 +17,8 @@ const CodeEditor = ({ problem }) => {
   return (
     <div className="bg-code-darkerpurple h-full">
       <CodeMirror
-        extensions={[cpp()]}
-        // extensions={[python()]}
+        // extensions={[cpp()]}
+        extensions={[python()]}
         // extensions={[javascript({ jsx: true })]}
         value={problem.boilerplate}
         onChange={onChange}
